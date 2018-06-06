49045eae85ebd5c040a1d99be47dda43b1af6407
Nathan Smith <nathan@nathansmith.io>
2018-06-06 13:31:16 -0700

    add landing page

---
+++ src/components/LandingPage/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const LandingPage = () => (
+  <div>
+    <div>Daily Bruin 2018</div>
+    <h1>Graduation Issue</h1>
+    <div>line</div>
+    <nav>
+      <Link>News</Link>
+      <Link>Sports</Link>
+      <Link>Arts</Link>
+      <Link>Opinion</Link>
+      <Link>Photo</Link>
+      <Link>Illustrations</Link>
+      <Link>Graphics</Link>
+      <Link>Video</Link>
+      <Link>Prime</Link>
+      <Link>The Quad</Link>
+      <Link>-30-</Link>
+    </nav>
+  </div>
+);
