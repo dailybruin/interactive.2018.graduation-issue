a5e9f21496f7cbf1075a6bb4cb96867ada1ab3b3
dustinnewman98 <dustinnewman98@gmail.com>
2018-06-06 17:50:20 -0700

    added arm date

--- src/components/timeline/timelineBlock.tsx
+++ src/components/timeline/timelineBlock.tsx
@@ -15,7 +15,7 @@ class TimelineBlock extends React.Component<{}, {}> {
         return (
             // arm of timeline
             <div className="timelineBlock">
-                <TimelineArm color={this.props.color} />
+                <TimelineArm color={this.props.color} date="Sept 2014"/>
                 <TimelineItem borderColor={this.props.color} headline='song of the year' song='Style' artist='Taylor Swift' imgSrc='https://is1-ssl.mzstatic.com/image/thumb/Music3/v4/2c/b1/87/2cb187fa-2da2-839e-436a-8997561c0493/UMG_cvrart_00843930013562_01_RGB72_1400x1400_14UMDIM03405.jpg/268x0w.jpg' />
             </div>
         );
