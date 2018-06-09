6e65fae63ba9cddf94c9d752bd4e8ed7c567ba4d
Nathan Smith <nathan@nathansmith.io>
2018-06-09 14:50:28 -0700

    Section grid (#11)
    * start
    
    * add grid
    
    * Getting there
    
    * Add -30- explainer text

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,34 +1,28 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Link } from '@reach/router';
-import glamorous, {ThemeProvider} from 'glamorous';
+import glamorous, { ThemeProvider } from 'glamorous';
 import './navBar.css';
 
 import { H1 } from 'glamorous';
 
-function NavigationBar() {
-    return (
-        <div id="navbar">
-            <img src="" alt="" />
-            <H1
-                id="gradIssueH1">
-                graduation issue
-            </H1>
-            <nav id="nav">
-                <Link to="">News</Link>
-                <Link to="">Sports</Link>
-                <Link to="">Arts</Link>
-                <Link to="">Opinion</Link>
-                <Link to="">Photo</Link>
-                <Link to="">Illustrations</Link>
-                <Link to="">Graphics</Link>
-                <Link to="">Video</Link>
-                <Link to="">Prime</Link>
-                <Link to="">The Quad</Link>
-                <Link to="">-30-</Link>
-            </nav>
-        </div>
-    );
-};
-
-export default NavigationBar;
+export default function NavigationBar() {
+  return (
+    <div id="navbar">
+      <img src="" alt="" />
+      <H1 id="gradIssueH1">graduation issue</H1>
+      <nav id="nav">
+        <a href="">News</a>
+        <a href="">Sports</a>
+        <a href="">Arts</a>
+        <a href="">Opinion</a>
+        <a href="">Photo</a>
+        <a href="">Illustrations</a>
+        <a href="">Graphics</a>
+        <a href="">Video</a>
+        <a href="">Prime</a>
+        <a href="">The Quad</a>
+        <a href="">-30-</a>
+      </nav>
+    </div>
+  );
+}
--- src/components/SectionGrid/index.tsx
+++ src/components/SectionGrid/index.tsx
@@ -1,21 +1,51 @@
 import * as React from 'react';
+import { Div } from 'glamorous';
+import { Story } from '@/components/App';
 
-export default function SectionGrid(section, stories) {
-  const gridItems = stories.map(story => (
-    <article>
-      <figure>
-        <img src={story.image} alt={story.alt} />
-        <figcaption>{story.credits}</figcaption>
-      </figure>
-      <h1>{story.headline}</h1>
-      <div>{story.byline}</div>
-    </article>
-  ));
+export default function SectionGrid({ sections, stories }) {
+  if (sections.length > 0 && stories.length > 0) {
+    console.log(sections);
+    const sectionsJSX = sections.map(section => {
+      const storiesJSX = stories
+        .filter(story => story.section === section)
+        .map(story => (
+          <a href={story.link}>
+            <article>
+              <figure>
+                <img src={story.imageurl} alt={story.imagedescription} />
+                <figcaption>{story.imagecredits}</figcaption>
+              </figure>
+              <h1>{story.headline}</h1>
+              <span>{story.author}</span>
+            </article>
+          </a>
+        ));
+      return (
+        <div>
+          <h2>{section}</h2>
+          {section === '-30-' && (
+            <div>
+              Back in the day, newspaper publications would send their pieces
+              via telegraph. To signify the end of a telegram, publications
+              would use character sequences such as “-30-.” Those times have
+              changed, but student publications like the Daily Bruin continue to
+              provide for their campuses and serve as a place for students to
+              call home. Read these graduating staffers’ personal columns, where
+              they share their final messages before leaving The Bruin.
+            </div>
+          )}
+          <Div
+            display="grid"
+            grid-template-columns="repeat(auto-fill, minmax(250px, 1fr))"
+            grid-gap="30px"
+          >
+            {storiesJSX}
+          </Div>
+        </div>
+      );
+    });
 
-  return (
-    <div>
-      <h2>{section}</h2>
-      <section>{gridItems}</section>
-    </div>
-  );
+    return <Div width="100vw">{sectionsJSX}</Div>;
+  }
+  return <h1>Loading...</h1>;
 }
