0f86046a06e6522097de9828cac7a772d0814868
dustinnewman98 <dustinnewman98@gmail.com>
2018-06-10 00:45:09 -0700

    landing pgae fixes again

--- src/components/ExplainerText/index.tsx
+++ src/components/ExplainerText/index.tsx
@@ -3,8 +3,11 @@ import './explainer-text.css';
 
 export default function ExplainerText() {
   return (
-    <p className="explainer-text">
-      Below is a timeline compiled by Bruin staffers.
+    <p id="explainer-text">
+      Below is a timeline of your past four years here at UCLA. Follow the Daily
+      Bruin's coverage of both local and national events over the past four
+      years. Scroll down to check out the rest of the Daily Bruin's content for
+      the end of the year.
     </p>
   );
 }
