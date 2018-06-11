aadb6b2644f0c905054f989d3937fa93e407c2ec
Nathan Smith <nathan@nathansmith.io>
2018-06-10 18:31:35 -0700

    Fix case sensitivity

---
+++ src/components/Timeline/TimelineItem/index.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import TimelineArm from './TimelineArm';
+import TimelineBox from '@/components/Timeline/TimelineItem/TimelineBox';
+import './timeline-item.scss';
+
+export enum TimelineItemType {
+  Article,
+  Movie,
+  Song,
+}
+
+interface TimelineItemProps {
+  type: TimelineItemType;
+  date: string;
+  image: string;
+  link: string;
+  headline?: string;
+  media?: string;
+  author: string;
+}
+
+export default class TimelineItem extends React.Component<
+  TimelineItemProps,
+  {}
+> {
+  getColor = () => {
+    switch (this.props.type) {
+      case TimelineItemType.Article:
+        return '#DFF3FE';
+      case TimelineItemType.Song:
+        return '#FFFCE1';
+      case TimelineItemType.Movie:
+        return '#F8E2CE';
+    }
+  };
+
+  render() {
+    return (
+      <div className="timeline-item">
+        <TimelineArm
+          color={this.getColor()}
+          date={this.props.date}
+          type={this.props.type}
+        />
+        <a href={this.props.link}>
+          <TimelineBox
+            type={this.props.type}
+            image={this.props.image}
+            headline={this.props.headline}
+            media={this.props.media}
+            author={this.props.author}
+          />
+        </a>
+      </div>
+    );
+  }
+}
---
+++ src/components/Timeline/index.tsx
@@ -0,0 +1,149 @@
+import * as React from 'react';
+import YearMarker from './YearMarker';
+import { default as TimelineItem, TimelineItemType } from './TimelineItem';
+import './timeline.scss';
+
+interface TimelineState {
+  data: any;
+}
+
+class Timeline extends React.Component<{}, TimelineState> {
+  scrolling: boolean;
+  timeline: any;
+  items: any;
+  arms: any;
+  boxes: any;
+  offset: number;
+
+  constructor(props) {
+    super(props);
+    this.scrolling = false;
+    this.timeline = undefined;
+    this.items = undefined;
+    this.arms = undefined;
+    this.boxes = undefined;
+    this.offset = 0.8;
+    this.state = { data: [] };
+  }
+
+  async componentWillMount() {
+    const response = await fetch(
+      'https://kerckhoff.dailybruin.com/api/packages/flatpages/interactive.2018.gradissue/'
+    );
+    const data = await response.json();
+    let aml = data.data['data.aml'];
+    const images = data.images;
+    console.log(aml);
+
+    for (let i = 0; i < aml.years.length; i += 1) {
+      aml.years[i].content.map((item, j) => {
+        const img = images.s3[item.image];
+        if (img) {
+          aml.years[i].content[j].image = img.url;
+        }
+      });
+    }
+    console.log(aml);
+
+    this.setState({
+      data: aml.years,
+    });
+  }
+
+  componentDidMount() {
+    this.init();
+    window.addEventListener('scroll', e => {
+      if (!this.scrolling) {
+        this.scrolling = true;
+        !window.requestAnimationFrame
+          ? setTimeout(() => this.checkTimelineScroll, 250)
+          : window.requestAnimationFrame(() => this.checkTimelineScroll());
+      }
+    });
+  }
+
+  init() {
+    this.timeline = document.getElementById('timeline');
+    this.items = document.getElementsByClassName('timeline-item');
+    this.arms = document.getElementsByClassName('timeline-arm');
+    this.boxes = document.getElementsByClassName('timeline-box');
+    this.hideBlocks();
+  }
+
+  checkTimelineScroll() {
+    this.showBlocks();
+    this.scrolling = false;
+  }
+
+  showBlocks() {
+    // if (!'classList' in document.documentElement) {
+    //   return;
+    // }
+    for (var i = 0; i < this.items.length; i++) {
+      if (
+        this.boxes[i].classList.contains('hidden') &&
+        this.items[i].getBoundingClientRect().top <=
+          window.innerHeight * this.offset
+      ) {
+        // add bounce-in animation
+        this.arms[i].classList.add('timeline-box--bounce-in');
+        this.boxes[i].classList.add('timeline-box--bounce-in');
+        this.arms[i].classList.remove('hidden');
+        this.boxes[i].classList.remove('hidden');
+      }
+    }
+  }
+
+  hideBlocks() {
+    //hide timeline blocks which are outside the viewport
+    // if (!'classList' in document.documentElement) {
+    //   return;
+    // }
+    for (var i = 0; i < this.items.length; i++) {
+      if (
+        this.items[i].getBoundingClientRect().top >
+        window.innerHeight * this.offset
+      ) {
+        this.arms[i].classList.add('hidden');
+        this.boxes[i].classList.add('hidden');
+      }
+    }
+  }
+
+  render() {
+    if (this.state.data.length === 0) {
+      return <div id="timeline">Loading...</div>;
+    } else {
+      console.log(this.state.data);
+      const events = this.state.data.map(year =>
+        year.content.map(story => (
+          <TimelineItem
+            type={TimelineItemType.Article}
+            date={story.date}
+            image={story.image}
+            author={story.author}
+            link={story.link}
+            headline={story.headline}
+          />
+        ))
+      );
+
+      return (
+        <div id="timeline">
+          <YearMarker year="2014" />
+          <div className="timeline-stem">{events[0]}</div>
+          <YearMarker year="2015" />
+          <div className="timeline-stem">{events[1]}</div>
+          <YearMarker year="2016" />
+          <div className="timeline-stem">{events[2]}</div>
+          <YearMarker year="2017" />
+          <div className="timeline-stem">{events[3]}</div>
+          <YearMarker year="2018" />
+          <div className="timeline-stem">{events[4]}</div>
+        </div>
+      );
+    }
+  }
+}
+
+export default Timeline;
