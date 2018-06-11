0283793fb7d5ca76b08a108e4ac23313c9d00902
dustinnewman98 <dustinnewman98@gmail.com>
2018-06-10 17:27:37 -0700

    landing anim integrated into react component

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -11,22 +11,122 @@ interface LandingPageProps {
 class LandingPage extends React.Component<LandingPageProps, {}> {
   constructor(props) {
     super(props);
-    this.landing = null;
+    this.landingDiv = null;
+    this.title = null;
+    this.nav = null;
+    this.navbar = null;
+    this.svg = null;
+    this.timeline = null;
   }
+
   componentDidMount() {
-    this.landing = document.getElementById('landingDiv');
-    window.addEventListener('scroll', this.handleScroll);
+    this.title = document.getElementById('gradIssueH1');
+    this.nav = document.getElementById('nav');
+    this.navbar = document.getElementById('navbar');
+    this.landingDiv = document.getElementById('landingDiv');
+    this.svg = document.getElementById('landingSvg');
+    this.timeline = document.getElementById('explainer-text');
+
+    if (window.innerWidth > 1010) {
+      console.log('calling handle scroll');
+      document.addEventListener('DOMContentLoaded', () =>
+        document.addEventListener('scroll', e => this.handleScroll(e))
+      );
+    } else {
+      document.addEventListener('DOMContentLoaded', () =>
+        this.handleScrollMobile(event)
+      );
+    }
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
+    if (window.innerWidth > 1010) {
+      window.removeEventListener('scroll', this.handleScroll);
+    } else {
+      window.removeEventListener('scroll', this.handleScrollMobile);
+    }
   }
 
-  handleScroll(event) {
-    // console.log(event);
-    // console.log(window.scrollY);
-    // this.landing.style.opacity = `${(100 - window.scrollY) * 0.01}`;
-  }
+  handleScroll = event => {
+    let s = window.scrollY;
+    console.log(s);
+    // 230 start
+    // 430 end
+    // 9.4/1 = 9.4
+    // 9.4/3.13 = 3
+    // 9.4/(x) = fontSize vw
+    // x = 0.01067*s - 1.453
+    if (s < 10) {
+      this.landingDiv.style.height = '100vh';
+      this.navbar.style.height = 'auto';
+      this.landingDiv.classList.remove('landingDivPost');
+      this.timeline.style.marginTop = '100vh';
+    } else if (s > 10 && s < 330) {
+      let v = -0.2813 * s + 102.8;
+      this.landingDiv.style.height = v + 'vh';
+      this.navbar.style.height = v + 'vh';
+      // 100 --> 10vh
+      let f = -0.0625 * s + 100.6;
+      //timeline.style.marginTop = f + 'vh';
+
+      let w = -0.3226 * s + 6.452;
+      this.svg.style.transform = 'translateY(' + w + '%)';
+      if (w < -72) {
+        this.svg.style.display = 'none';
+      } else {
+        this.svg.style.display = 'block';
+      }
+      // 0 --> -100%
+
+      let x = -0.02 * s + 9.6;
+      this.title.style.fontSize = x + 'vw';
+      // 9.4 --> 3vw
+
+      let y = 0.1906 * s - 2.906;
+      this.title.style.marginRight = y + '%';
+      // -1 --> 60%
+
+      let z = 0.0009375 * s - 0.009375;
+      this.title.style.marginTop = z + 'em';
+      // 0 --> 0.3em
+
+      this.landingDiv.style.justifyContent = 'center';
+    } else if (s >= 330) {
+      // navbar.style.height = '10vh';
+      this.svg.style.display = 'none';
+      this.landingDiv.style.height = '15vh';
+      this.landingDiv.style.zIndex = '2';
+      this.landingDiv.style.justifyContent = 'flex-start';
+      // landingDiv.style.background = 'inherit';
+      this.title.style.fontSize = '3vw';
+      this.title.style.marginRight = '60%';
+      this.title.style.marginTop = '0.3em';
+      this.nav.style.paddingTop = '0.3em';
+      this.timeline.style.marginTop = '90vh';
+      this.landingDiv.classList.add('landingDivPost');
+    }
+  };
+
+  handleScrollMobile = event => {
+    let s = window.scrollY;
+    if (s > 570) {
+      this.nav.style.display = 'none';
+      this.landingDiv.style.justifyContent = 'flex-start';
+      this.navbar.style.width = '100%';
+      this.navbar.style.textAlign = 'center';
+      this.navbar.style.padding = '0.5em 0';
+      this.navbar.style.zIndex = '3';
+      this.navbar.style.backgroundImage = 'inherit';
+      this.navbar.style.position = 'fixed';
+    } else {
+      this.nav.style.display = 'flex';
+      this.landingDiv.style.justifyContent = 'center';
+      this.navbar.style.backgroundImage = 'initial';
+      this.navbar.style.position = 'static';
+      this.navbar.style.width = '90%';
+      this.navbar.style.textAlign = 'right';
+    }
+  };
 
   render() {
     return (
