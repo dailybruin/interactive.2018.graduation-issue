de0e1f1eb215888102d379c47a2d35fb1551e9c8
Richard Yang <32914962+ryang72@users.noreply.github.com>
2018-11-16 15:45:03 -0800

    Adding Designer Credit to Footer (#38)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
         </div>
         <div>
           Built with Suzy’s <span className="love">♥</span> in Kerckhoff 118 by
-          Dustin Newman, Nathan Smith and Michael Zhang.
+          Ashley Miao, Callista Wu, Dustin Newman, Nathan Smith and Michael Zhang.
         </div>
       </div>
     </footer>
